ab28ac738ee915f444c8c1d0da5b60cad462d04c
Granita🍧 <tsofu.yeh@gmail.com>
2026-02-05 23:09:40 +0800

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿using Moq;
+
+namespace ConsoleApp1;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+    }
+
+    public class User
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+
+    public interface IUserService
+    {
+        User GetUser(string email, string  password);
+    }
+
+    public class UserService : IUserService
+    {
+        User IUserService.GetUser(string email, string password)
+        {
+            return new User();
+        }
+    }
+
+    public class LoginService
+    {
+        private readonly IUserService _userService;
+
+        public LoginService(IUserService service)
+        {
+            _userService = service;
+        }
+
+        public bool Login(string email, string password)
+        {
+            var user = _userService.GetUser(email, password);
+
+            return user != null;
+        }
+    }
+
+    public interface IUserInfo
+    {
+        string UserName { get; set; }
+        int Age { get; set; }
+
+        string GetUserData();
+    }
+
+    public interface IVerifyService
+    {
+        void Process(int value);
+    }
+
+    public class VerifyServiceClient
+    {
+        private readonly IVerifyService _service;
+
+        public VerifyServiceClient(IVerifyService service)
+        {
+            _service = service;
+        }
+
+        public void Execute(int[] values)
+        {
+            foreach (var value in values)
+            {
+                _service.Process(value);
+            }
+        }
+    }
+}
